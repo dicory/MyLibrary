19bd61d7d59d41eea9c5e8e94789c45a00746f89
igor <dicory2016@gmail.com>
2024-08-08 21:55:43 +0500

    start_1

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
-    public static void main (String[] args)
-    {
-        System.out.print(1);
+    public static void main (String[] args) {
+        System.out.print("StartProjectBook");
     }
 }
